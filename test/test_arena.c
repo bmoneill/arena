ca699d76f9470852baf05490bf5f6d43f62cac38
Ben O'Neill <ben@oneill.sh>
2026-05-08 23:49:27 -0400

    Fix test_arena_clear_managed

--- test/test_arena.c
+++ test/test_arena.c
@@ -292,7 +292,13 @@ void test_arena_clear_managed(void) {
 
     int result = arena_clear(arena);
     TEST_ASSERT_EQUAL(ARENA_SUCCESS, result);
-    for (size_t i = 0; i < arena->maxBlocks; i++) {
+
+    TEST_ASSERT_EQUAL(0, arena->head[0].idx);
+    TEST_ASSERT_EQUAL(0, arena->head[i].size);
+    TEST_ASSERT_EQUAL(ARENA_TAG_NONE, arena->head[i].tag);
+    TEST_ASSERT_EQUAL(ARENA_STATUS_FREE, arena->head[i].status);
+
+    for (size_t i = 1; i < arena->maxBlocks; i++) {
         TEST_ASSERT_EQUAL(-1, arena->head[i].idx);
         TEST_ASSERT_EQUAL(0, arena->head[i].size);
         TEST_ASSERT_EQUAL(ARENA_TAG_NONE, arena->head[i].tag);
